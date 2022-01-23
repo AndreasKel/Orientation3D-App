f4cc548d65aebc84520d9816875a877bd1c149ed
Andreas Kelepeniotis <kelepeniotisandreas@gmail.com>
2022-01-23 12:13:10 +0000

    Image changed

--- Orientation3D/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ Orientation3D/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -69,7 +69,7 @@ namespace Orientation3D {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Orientation3D;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Orientation3D;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
